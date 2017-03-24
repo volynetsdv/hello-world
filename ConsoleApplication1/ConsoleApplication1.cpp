3a49b3b358ece87edc23dffa688fa9bc15ee9c7e
Дмитрий Волынец < >
2017-03-24 15:01:26 +0200

    my first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+int main()
+{
+	using namespace std;
+	cout << "Get start to learn C++";
+	cout << endl;
+	cout << "bla bla bla" << endl;
+	cin.get();
+    return 0;
+}
